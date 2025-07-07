49b86244b522679560cdeb872636bc866cbdec29
LeVietAnh <vianhofico@gmail.com>
2025-07-08 00:53:35 +0700

    up prototype for header, footer, login, register, forgot-password

--- HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/dto/response/ProductImageDTO.java
+++
@@ -1,4 +0,0 @@
-package clothes.hsf302_group3_project.dto.response;
-
-public class ProductImageDTO {
-}
--- HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package clothes.hsf302_group3_project.repository;
-
-import clothes.hsf302_group3_project.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
