af7cf1e16267c7b8ec28d370c9c35e572c805961
tungtq123 <he182488toquoctung@gmail.com>
2025-07-13 21:50:17 +0700

    Update CartItemServiceImpl.java

--- HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/service/Impl/CartItemServiceImpl.java
+++ HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/service/Impl/CartItemServiceImpl.java
@@ -1,6 +1,23 @@
 package clothes.hsf302_group3_project.service.Impl;
 
+import clothes.hsf302_group3_project.entity.CartItem;
+import clothes.hsf302_group3_project.repository.CartItemRepository;
 import clothes.hsf302_group3_project.service.CartItemService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CartItemServiceImpl implements CartItemService {
+    private final CartItemRepository cartItemRepository;
+
+    public CartItemServiceImpl(CartItemRepository cartItemRepository) {
+        this.cartItemRepository = cartItemRepository;
+    }
+
+    @Override
+    public List<CartItem> findByIdsAndUserId(List<Long> ids, Long userId) {
+        return cartItemRepository.findByIdInAndCart_User_Id(ids, userId);
+    }
+
 }
