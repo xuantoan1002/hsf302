7688f0823dd6e11b87fe9352d384d5310128f40e
tungtq123 <he182488toquoctung@gmail.com>
2025-07-13 21:53:56 +0700

    Update ConverterDTO.java

--- HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/converter/ConverterDTO.java
+++ HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/converter/ConverterDTO.java
@@ -50,4 +50,35 @@ public class ConverterDTO {
         dto.setQuantity(productSize.getQuantity());
         return dto;
     }
+
+public UserDTO convertToUserDTO(User user) {
+        UserDTO dto = new UserDTO();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        dto.setPassword(user.getPassword());
+        dto.setPhone(user.getPhone());
+        dto.setRole(user.getRole());
+        dto.setCreatedAt(user.getCreatedAt());
+        return dto;
+    }
+
+    public CartDTO convertToCartDTO(Cart cart) {
+        CartDTO dto = new CartDTO();
+        dto.setId(cart.getId());
+        dto.setSum(cart.getSum());
+        dto.setUser(convertToUserDTO(cart.getUser()));
+        return dto;
+    }
+
+    public CartItemDTO convertToCartItemDTO(CartItem cartItem) {
+        CartItemDTO dto = new CartItemDTO();
+        dto.setId(cartItem.getId());
+        dto.setCart(convertToCartDTO(cartItem.getCart()));
+        dto.setQuantity(cartItem.getQuantity());
+        dto.setPrice(cartItem.getPrice());
+        dto.setProduct(convertToProductDTO(cartItem.getProduct()));
+        return dto;
+    }
+    
 }
