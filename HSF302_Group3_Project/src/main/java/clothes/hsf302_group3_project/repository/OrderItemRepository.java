c02783800c4a23123ea54ada3ee978268e796398
Tên của bạn <q314e25r@gmail.com>
2025-07-20 18:51:09 +0700

    add again

--- HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/repository/OrderItemRepository.java
+++ HSF302_Group3_Project/src/main/java/clothes/hsf302_group3_project/repository/OrderItemRepository.java
@@ -3,5 +3,11 @@ package clothes.hsf302_group3_project.repository;
 import clothes.hsf302_group3_project.entity.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    // THÊM DÒNG NÀY
+    List<OrderItem> findByOrderId(Long orderId);
+
 }
